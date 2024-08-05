ee171d412f35e5aca5814735a1e226e0b7c5e866
Thiago Vanzele <thiagovanzele@gmail.com>
2024-08-05 18:47:02 -0300

    feat(model) validacao de telefone para MedicoDto

--- src/main/java/sonda/med/app/model/entities/dto/MedicoDto.java
+++ src/main/java/sonda/med/app/model/entities/dto/MedicoDto.java
@@ -2,9 +2,10 @@ package sonda.med.app.model.entities.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
-public record MedicoDto(@NotBlank String nome, @NotBlank @Email String email, @NotBlank String telefone, @NotBlank @Pattern(regexp = "\\d{4,6}") String crm,
-		@NotBlank String especialidade, @NotBlank String cep, @NotBlank String numero) {
+public record MedicoDto(@NotBlank String nome, @NotBlank @Email String email, @NotBlank @Pattern(regexp = "\\+?\\d{10,15}") String telefone, @NotBlank @Pattern(regexp = "\\d{4,7}") String crm,
+		@NotNull String especialidade, @NotBlank String cep, @NotBlank String numero) {
 
 }
