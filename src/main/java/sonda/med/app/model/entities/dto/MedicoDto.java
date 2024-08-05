17cba27966c34de854944dac119533c0cf3ef20b
Thiago Vanzele <thiagovanzele@gmail.com>
2024-08-05 18:25:59 -0300

    feat(model) nova validacao para MedicoDto

--- src/main/java/sonda/med/app/model/entities/dto/MedicoDto.java
+++ src/main/java/sonda/med/app/model/entities/dto/MedicoDto.java
@@ -1,7 +1,10 @@
 package sonda.med.app.model.entities.dto;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
-public record MedicoDto(@NotNull String nome, @NotNull String email,@NotNull String telefone,@NotNull String crm,@NotNull String especialidade,@NotNull String cep,@NotNull String numero) {
+public record MedicoDto(@NotBlank String nome, @NotBlank @Email String email, @NotBlank String telefone, @NotBlank @Pattern(regexp = "\\d{4,6}") String crm,
+		@NotBlank String especialidade, @NotBlank String cep, @NotBlank String numero) {
 
 }
