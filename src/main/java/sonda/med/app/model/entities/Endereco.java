72df1ff091498a97eb8c25f260c3c7fed75bd904
Thiago Vanzele <thiagovanzele@gmail.com>
2024-08-05 16:30:49 -0300

    style(model): Retirado imports incorretos de Endereco

--- src/main/java/sonda/med/app/model/entities/Endereco.java
+++ src/main/java/sonda/med/app/model/entities/Endereco.java
@@ -8,15 +8,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Table(name = "tb_endereco")
 @Entity
 public class Endereco implements Serializable {
