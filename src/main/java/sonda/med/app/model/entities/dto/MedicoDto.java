dddd07f0fe1b5b74507d23fccf5732872d4b74aa
Thiago Vanzele <thiagovanzele@gmail.com>
2024-08-05 17:26:45 -0300

    feat(model): novo atributo em MedicoDto

--- src/main/java/sonda/med/app/model/entities/dto/MedicoDto.java
+++ src/main/java/sonda/med/app/model/entities/dto/MedicoDto.java
@@ -1,5 +1,5 @@
 package sonda.med.app.model.entities.dto;
 
-public record MedicoDto(String nome, String email, String telefone, String Crm, String cep) {
+public record MedicoDto(String nome, String email, String telefone, String crm, String especialidade, String cep, String numero) {
 
 }
