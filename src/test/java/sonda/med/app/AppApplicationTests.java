1eae49e489e2bbbba9d7f80f10ebf2d2640fe092
Thiago Vanzele <thiagovanzele@gmail.com>
2024-08-05 13:04:12 -0300

    Projeto criado

---
+++ src/test/java/sonda/med/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package sonda.med.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
