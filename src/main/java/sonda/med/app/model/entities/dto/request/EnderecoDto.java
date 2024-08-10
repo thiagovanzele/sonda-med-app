fb590e6572c4db913c4b4dd3e89d6a69364b68f0
Thiago Vanzele <thiagovanzele@gmail.com>
2024-08-09 21:13:08 -0300

    refactor: enderecoDto movido para pacote de 'request'

--- src/main/java/sonda/med/app/model/entities/dto/request/EnderecoDto.java
+++ src/main/java/sonda/med/app/model/entities/dto/request/EnderecoDto.java
@@ -1,4 +1,4 @@
-package sonda.med.app.model.entities.dto;
+package sonda.med.app.model.entities.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
