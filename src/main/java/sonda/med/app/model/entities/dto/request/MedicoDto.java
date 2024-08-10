8a1b709df631d05abec4688371fe8e8d657e2c4a
Thiago Vanzele <thiagovanzele@gmail.com>
2024-08-09 21:11:40 -0300

    refactor: melhorado validacoes de medico

--- src/main/java/sonda/med/app/model/entities/dto/request/MedicoDto.java
+++ src/main/java/sonda/med/app/model/entities/dto/request/MedicoDto.java
@@ -1,11 +1,15 @@
-package sonda.med.app.model.entities.dto;
+package sonda.med.app.model.entities.dto.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 
-public record MedicoDto(@NotBlank String nome, @NotBlank @Email String email, @NotBlank @Pattern(regexp = "\\+?\\d{10,15}") String telefone, @NotBlank @Pattern(regexp = "\\d{4,7}") String crm,
-		@NotNull String especialidade, @NotBlank String cep, @NotBlank String numero) {
-
+public record MedicoDto(
+		@NotBlank(message = "O nome não pode estar em branco") String nome,
+		@NotBlank(message = "O email não pode estar em branco") @Email(message = "O email deve ser válido") String email,
+		@NotBlank(message = "O telefone não pode estar em branco") @Pattern(regexp = "\\+?\\d{10,15}", message = "O telefone deve corresponder ao formato: +1234567890 ou 1234567890") String telefone,
+		@NotBlank(message = "O CRM não pode estar em branco") String crm,
+		@NotBlank(message = "A especialidade não pode estar em branco") String especialidade,
+		@NotBlank(message = "O CEP não pode estar em branco") @Pattern(regexp = "\\d{8}", message = "O CEP deve conter 8 digitos") String cep,
+		@NotBlank(message = "O número não pode estar em branco") String numero) {
 }
