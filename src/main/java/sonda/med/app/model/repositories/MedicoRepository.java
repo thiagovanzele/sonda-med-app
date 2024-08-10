639e29972329c32570b8252929ffab67fdcdaf25
Thiago Vanzele <thiagovanzele@gmail.com>
2024-08-10 14:19:38 -0300

    feat: adicionado novo metodo a MedicoRepository para listar medicos ativos

--- src/main/java/sonda/med/app/model/repositories/MedicoRepository.java
+++ src/main/java/sonda/med/app/model/repositories/MedicoRepository.java
@@ -1,9 +1,12 @@
 package sonda.med.app.model.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import sonda.med.app.model.entities.Medico;
 
 public interface MedicoRepository extends JpaRepository<Medico, Long>{
 
+	Page<Medico> findAllByAtivoTrue(Pageable paginacao);
 }
