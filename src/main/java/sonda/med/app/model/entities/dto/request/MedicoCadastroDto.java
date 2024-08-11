bb1333a2c31b20d119880a17e843d731fa06d2fd
Thiago Vanzele <thiagovanzele@gmail.com>
2024-08-11 16:22:13 -0300

    refactor(model) melhorado nome do Dto para enviar cadastro de medico

--- src/main/java/sonda/med/app/model/entities/dto/request/MedicoCadastroDto.java
+++ src/main/java/sonda/med/app/model/entities/dto/request/MedicoCadastroDto.java
@@ -3,8 +3,9 @@ package sonda.med.app.model.entities.dto.request;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
+import sonda.med.app.model.entities.Medico;
 
-public record MedicoDto(
+public record MedicoCadastroDto(
 		@NotBlank(message = "O nome não pode estar em branco") String nome,
 		@NotBlank(message = "O email não pode estar em branco") @Email(message = "O email deve ser válido") String email,
 		@NotBlank(message = "O telefone não pode estar em branco") @Pattern(regexp = "\\+?\\d{10,15}", message = "O telefone deve corresponder ao formato: +1234567890 ou 1234567890") String telefone,
@@ -12,4 +13,9 @@ public record MedicoDto(
 		@NotBlank(message = "A especialidade não pode estar em branco") String especialidade,
 		@NotBlank(message = "O CEP não pode estar em branco") @Pattern(regexp = "\\d{8}", message = "O CEP deve conter 8 digitos") String cep,
 		@NotBlank(message = "O número não pode estar em branco") String numero) {
+	
+	
+	public MedicoCadastroDto(Medico medico) {
+		this(medico.getNome(), medico.getEmail(), medico.getTelefone(), medico.getCrm(), medico.getEspecialidade().name(), medico.getEndereco().getCep(), medico.getEndereco().getNumero());
+	}
 }
