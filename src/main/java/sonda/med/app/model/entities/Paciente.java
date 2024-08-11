bca7d8f856be97a2392e0bd56bacbcb693044caa
Thiago Vanzele <thiagovanzele@gmail.com>
2024-08-11 16:28:42 -0300

    feat(model) entidade Paciente criada

---
+++ src/main/java/sonda/med/app/model/entities/Paciente.java
@@ -0,0 +1,100 @@
+package sonda.med.app.model.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_paciente")
+public class Paciente implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	private String email;
+	
+	private String CPF;
+	
+	private Endereco endereco;
+
+	public Paciente() {
+		super();
+	}
+
+	public Paciente(String nome, String email, String cPF, Endereco endereco) {
+		super();
+		this.nome = nome;
+		this.email = email;
+		CPF = cPF;
+		this.endereco = endereco;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCPF() {
+		return CPF;
+	}
+
+	public void setCPF(String cPF) {
+		CPF = cPF;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(CPF, id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Paciente other = (Paciente) obj;
+		return Objects.equals(CPF, other.CPF) && Objects.equals(id, other.id);
+	}
+	
+	
+
+}
