45a20a0d0aa35161c7a66a7ba345898b6eab724b
Thiago Vanzele <thiagovanzele@gmail.com>
2024-08-19 19:09:11 -0300

    feat: melhor tratamento de excecoes

--- src/main/java/sonda/med/app/controllers/exceptions/ControllerExceptionHandler.java
+++ src/main/java/sonda/med/app/controllers/exceptions/ControllerExceptionHandler.java
@@ -4,12 +4,19 @@ import java.time.Instant;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.naming.AuthenticationException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.auth0.jwt.exceptions.JWTDecodeException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.ValidationException;
@@ -52,6 +59,51 @@ public class ControllerExceptionHandler {
 	public ResponseEntity<StandardError> validationException(ValidationException e, HttpServletRequest request) {
 		String error = "Erro de validacao";
 		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, request.getRequestURI());
+		err.setMensagem(e.getMessage());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<StandardError> erroBadCredentials(BadCredentialsException e, HttpServletRequest request) {
+		String error = "Credênciais inválidas";
+		HttpStatus status = HttpStatus.UNAUTHORIZED;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, request.getRequestURI());
+		err.setMensagem(e.getMessage());
+        return ResponseEntity.status(status).body(err);
+    }
+	
+	@ExceptionHandler(AuthenticationException.class)
+	public ResponseEntity<StandardError> authenticationException(AuthenticationException e, HttpServletRequest request) {
+		String error = "Falha na autenticacao";
+		HttpStatus status = HttpStatus.UNAUTHORIZED;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, request.getRequestURI());
+		err.setMensagem(e.getMessage());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(JWTVerificationException.class)
+	public ResponseEntity<StandardError> tokenValidationException(JWTVerificationException e, HttpServletRequest request) {
+		String error = "Falha na autenticacao";
+		HttpStatus status = HttpStatus.UNAUTHORIZED;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, request.getRequestURI());
+		err.setMensagem(e.getMessage());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(JWTDecodeException.class)
+	public ResponseEntity<StandardError> tokenValidationException(JWTDecodeException e, HttpServletRequest request) {
+		String error = "Token invalido";
+		HttpStatus status = HttpStatus.UNAUTHORIZED;
+		StandardError err = new StandardError(Instant.now(), status.value(), error, request.getRequestURI());
+		err.setMensagem(e.getMessage());
+		return ResponseEntity.status(status).body(err);
+	}
+	
+	@ExceptionHandler(AccessDeniedException.class)
+	public ResponseEntity<StandardError> tokenValidationException(AccessDeniedException e, HttpServletRequest request) {
+		String error = "Acesso negado";
+		HttpStatus status = HttpStatus.FORBIDDEN;
 		StandardError err = new StandardError(Instant.now(), status.value(), error, request.getRequestURI());
 		err.setMensagem(e.getMessage());
 		return ResponseEntity.status(status).body(err);
