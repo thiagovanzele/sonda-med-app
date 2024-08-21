e6bf14fcb188ecb8d59bcd2d4f754bff4e77b250
Thiago Vanzele <thiagovanzele@gmail.com>
2024-08-21 18:14:30 -0300

    chore: alterado a data e hora para Sao Paulo

--- src/main/java/sonda/med/app/AppApplication.java
+++ src/main/java/sonda/med/app/AppApplication.java
@@ -1,5 +1,8 @@
 package sonda.med.app;
 
+import java.time.ZoneId;
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AppApplication {
 
 	public static void main(String[] args) {
+		TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("America/Sao_Paulo")));
 		SpringApplication.run(AppApplication.class, args);
 	}
 
--- src/main/java/sonda/med/app/config/JsonConfig.java
+++ src/main/java/sonda/med/app/config/JsonConfig.java
@@ -1,13 +1,22 @@
 package sonda.med.app.config;
 
+import java.util.TimeZone;
+
+import org.springframework.context.annotation.Bean;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class JsonConfig {
-	
+
+	@Bean
 	public ObjectMapper objectMapper() {
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
-		return mapper;
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JavaTimeModule());
+		objectMapper.configure(SerializationFeature.ORDER_MAP_ENTRIES_BY_KEYS, true);
+		objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		objectMapper.setTimeZone(TimeZone.getTimeZone("America/Sao_Paulo"));
+		return objectMapper;
 	}
 }
